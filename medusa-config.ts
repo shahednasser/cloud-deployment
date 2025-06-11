70a42ffcf8e0f8cc47d9a37d0f767b22b97620cf
Shahed Nasser <shahednasser@gmail.com>
2025-06-11 16:33:41 +0300

    add s3

--- medusa-config.ts
+++ medusa-config.ts
@@ -13,20 +13,26 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
   },
-  // modules: [
-  //   {
-  //     resolve: "@medusajs/medusa/file",
-  //     options: {
-  //       providers: [
-  //         {
-  //           resolve: "@medusajs/medusa/file-s3",
-  //           id: "s3",
-  //           options: {}
-  //         },
-  //       ],
-  //     },
-  //   },
-  // ],
+  modules: [
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              prefix: process.env.S3_PREFIX,
+              file_url: process.env.S3_FILE_URL,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+            },
+          },
+        ],
+      },
+    },
+  ],
   plugins: [
     {
       resolve: "@medusajs/loyalty-plugin",
